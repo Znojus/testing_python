3751b2da02a232eae870a1bac682579cd6b98786
Znojus <150673971+Znojus@users.noreply.github.com>
2026-03-13 12:14:32 +0200

    Add a minimal POST for login, shows user

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 @app.route('/')
@@ -21,4 +21,8 @@ def index():
 @app.route('/login')
 def login():
     form = LoginForm()
-    return render_template('login.html', title="Sign In", form=form)
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
