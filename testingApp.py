7608d442d570a68cc432a8d87a52db2d981d27f5
Znojus <150673971+Znojus@users.noreply.github.com>
2026-03-16 17:12:37 +0200

    Add shell context

--- testingApp.py
+++ testingApp.py
@@ -1,1 +1,8 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Task, Submission, TestCase
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Task': Task, 'Submission': Submission, 'TestCase': TestCase} #add the Submissio nand Testcase
