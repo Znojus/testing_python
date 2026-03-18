945867de80fdcc160d4ad3b5a660a3640c3fd1d0
Znojus <150673971+Znojus@users.noreply.github.com>
2026-03-18 15:54:05 +0200

    Add a login requirement, change the login() to include redirect to next

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db) # migration engine
 login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
